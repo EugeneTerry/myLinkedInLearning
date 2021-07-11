9fb2571faeaa207a415b0c62faa6e392a3886acb
EugeneTerry <eugene.terry73@gmail.com>
2021-07-11 13:09:06 -0500

    first commit with code

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,20 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const nightstand = {
+  color: "brown",
+  drawers: 3,
+  topDrawer: "wires",
+  middleDrawer: {
+    aaaBatteries: 15,
+    aaBatteries: 10
+  },
+  bottonDrawer: "hats",
+  drawerOpen: false,
+  toggleDrawer(drawerStatus) {
+    this.drawerOpen = drawerStatus;
+  }
+}
+
